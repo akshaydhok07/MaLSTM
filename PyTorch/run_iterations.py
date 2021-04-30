af17a1c1be1b14a53dc37f856b48c091cb8e809c
akshaydhok07 <65851551+akshaydhok07@users.noreply.github.com>
2021-05-01 00:40:58 +0530

    Update run_iterations.py

--- PyTorch/run_iterations.py
+++ PyTorch/run_iterations.py
@@ -8,7 +8,38 @@ import numpy as np
 
 from nltk import bleu_score
 
-from helper import Helper
+import time
+import math
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+class Helper(object):
+    def __init__(self):
+        pass
+
+    def as_minutes(self, s):
+        m = math.floor(s / 60)
+        s -= m * 60
+        return '%dm %ds' % (m, s)
+
+    def time_slice(self, since, percent):
+        now = time.time()
+        s = now - since
+        es = s / (percent)
+        rs = es - s
+        return '%s (- %s)' % (self.as_minutes(s), self.as_minutes(rs))
+
+    def show_plot(self, points):
+        plt.figure()
+        fig, ax = plt.subplots()
+        # this locator puts ticks at regular intervals
+        loc = ticker.MultipleLocator(base=0.2)
+        ax.yaxis.set_major_locator(loc)
+        plt.plot(points)
+
 
 class Run_Iterations(object):
     def __init__(self, data_name, model, x_train, y_train, index2word, batch_size, num_iters,
